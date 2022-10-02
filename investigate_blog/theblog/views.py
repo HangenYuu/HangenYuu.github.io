3b5d2ef34a2eee379d2e657ca22fa7695262597d
HangenYuu <97870091+HangenYuu@users.noreply.github.com>
2022-10-02 17:46:33 +0800

    changed base.html

--- investigate_blog/theblog/views.py
+++ investigate_blog/theblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+def waiting(request):
+    return render(request, 'waiting.html', {})
